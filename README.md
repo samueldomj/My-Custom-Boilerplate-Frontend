128034366afcf898c7e42b2d306d8cb3feff8309
Samuel Domínguez <sdominguez@csgroup.mx>
2020-08-30 00:03:09 -0500

    Primer commit de My Custom Boilerplat Frontend

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Custom Boilertplate Frontend 🤘🏽
---
+++ gulpfile.babel.js
@@ -0,0 +1,62 @@
+import gulp from 'gulp'
+import plumber from 'gulp-plumber'
+import browserSync from 'browser-sync'
+import sass from 'gulp-sass'
+import postcss from 'gulp-postcss'
+import cssnano from 'cssnano'
+import autoprefixer from 'autoprefixer'
+import babel from 'gulp-babel'
+import imagemin from 'gulp-imagemin'
+
+const server = browserSync.create()
+
+const postcssPlugins = [
+    cssnano(),
+    autoprefixer()
+]
+
+// Scss
+gulp.task('styles', () => {
+    return gulp.src('./src/scss/style.scss')
+        .pipe(plumber())
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(server.stream())
+})
+
+// Js
+gulp.task('js', () => {
+    return gulp.src('./src/js/*.js')
+        .pipe(plumber())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(server.stream())
+})
+
+gulp.task('images', () => {
+    return gulp.src('./public/images/**/**')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('default', () => {
+    server.init({
+        server: './public'
+    })
+
+    // HTML
+    gulp.watch('./public/*.html').on('change', server.reload)
+
+    // Scss
+    gulp.watch('./src/scss/**/*.scss', gulp.series('styles'))
+
+    // Js
+    gulp.watch('./src/js/*.js', gulp.series('js')).on('change', server.reload)
+})
